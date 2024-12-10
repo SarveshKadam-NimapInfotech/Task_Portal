bfc88f422a16b65f286011d3fc5594834cc8fc55
Surajjaiswal2002 <surajjaiswal@nimapinfotech.com>
2024-12-10 12:27:35 +0530

    adding deployment files

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Use the official .NET SDK image for building the application
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /app
+
+# Copy and restore dependencies
+COPY . .
+RUN dotnet restore
+
+# Build the application
+RUN dotnet publish -c Release -o out
+
+# Use the runtime-only image to run the application
+FROM mcr.microsoft.com/dotnet/aspnet:7.0
+WORKDIR /app
+COPY --from=build /app/out .
+
+# Expose the port the app will run on
+# EXPOSE 80
+
+# Start the application
+ENTRYPOINT ["dotnet", "Task_Portal.API.dll"]
--- Task_Portal/Program.cs
+++ Task_Portal/Program.cs
@@ -106,15 +106,13 @@ namespace Task_Portal.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task Portal API V1");
                 });
-            }
+            
 
             app.UseHangfireDashboard();
             RecurringJob.AddOrUpdate<ReminderJob>("send-reminder-emails", job => job.Execute(), Cron.Minutely);
